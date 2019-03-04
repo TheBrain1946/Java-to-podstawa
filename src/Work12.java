1aa27264409badc0fd1a78b31516485c400965d0
c-brisza <brian.szarek@wp.pl>
2019-03-04 14:44:50 +0100

    Work

---
+++ src/Work12.java
@@ -0,0 +1,20 @@
+public class Work12 {
+    public static void main(String[] args) {
+
+        int [] data = new int[4];
+        String [] name = new String[2];
+
+        data [0] = 1922;
+        data [1] = 2;
+        data [2] = 12;
+        data [3] = 50000;
+
+        name [0] = "Stefan";
+        name [1] = " Biodro";
+
+        System.out.println(data [0]+ "-"+ data [1]+ "-"+ data [2]+  " Zbiegł wiezień  " + name [0] + name [1] );
+
+        System.out.println("Wyznaczona nagroda $ " + data [data.length-1]);
+
+    }
+}
