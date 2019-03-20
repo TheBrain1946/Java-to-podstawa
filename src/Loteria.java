8cb2650e7e60693a10183b85e3a82a6e83caa6a8
TheBrain1946 <brian.szarek@wp.pl>
2019-03-20 09:18:13 +0100

    Lesson

--- src/Loteria.java
+++ src/Loteria.java
@@ -6,23 +6,23 @@ public class Loteria {
         Scanner input = new Scanner (System.in);
         Random random = new Random();
 
-        int [] wprowadzona  = new int[6];
+        int [] wprowadzone  = new int[6];
         System.out.println("Wprowadź 6 liczb od 1 do 24");
 
-        for (int i = 0; i < wprowadzona.length ; i++ ){
+        for (int i = 0; i < wprowadzone.length ; i++ ){
 
             boolean nieprawidlowa;
             do {
                 System.out.println("Wprowadź liczbe " + (i +1) );
-                wprowadzona [i] = input.nextInt();
-                 nieprawidlowa = wprowadzona [i] > 24 || wprowadzona [i] < 1;
+                wprowadzone [i] = input.nextInt();
+                 nieprawidlowa = wprowadzone [i] > 24 || wprowadzone [i] < 1;
                 if(nieprawidlowa) {
                     System.out.println("#Wprowazono nieprawidłową liczbę.");
                 }
                 }while (nieprawidlowa );
 
         }System.out.print("Wybrane liczby :");
-        for (int i : wprowadzona) {
+        for (int i : wprowadzone) {
             System.out.print(i+ " " );
 
         }
@@ -34,6 +34,44 @@ public class Loteria {
             System.out.print(wylosowane [i] + " " );
 
         }
+        int licznik = 0;
 
+        for (int wylosowana  : wylosowane) {
+            for (int wprowadzona  : wprowadzone) {
+                if (wylosowana == wprowadzona) {
+                    licznik++;
+                    System.out.println("\nTrafiona liczba:" +wprowadzona);
+
+                }
+            }
+        }
+        System.out.println(licznik +" :Trafionych liczb");
+
+        int nagorda =0;
+
+        switch (licznik){
+            case 0:
+                nagorda = 0;
+                break;
+            case 1:
+                nagorda = 0;
+                break;
+            case 2:
+                nagorda = 0;
+                break;
+            case 3:
+                nagorda = 16;
+                break;
+            case 4:
+                nagorda = 200;
+                break;
+            case 5:
+                nagorda = 4_000;
+                break;
+            case 6:
+                nagorda = 1_500_000;
+                break;
+        }
+        System.out.println("Twoja nagroda:" +nagorda+ "zł");
     }
 }
